7d840ac8d8527383865f32f1a3290ef794209bf3
SenaniDananjali <senanidananjali@gmail.com>
2017-12-29 17:31:39 +0530

    -stylist skills retrieve from backend

---
+++ src/getStylistSkills.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * User: senani
+ * Date: 12/29/17
+ */
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hairb2b";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+
+if($conn === false){
+  die("ERROR: Could not connect. " . $conn->connect_error);
+}
+
+$details = "SELECT stylist.id as sty , skills.description as skill FrOM stylist, stylistSkillMapping, skills
+    WHERE stylistSkillMapping.skill_id = skills.id && stylist.id = stylistSkillMapping.stylist_id";
+
+$result = $conn->query($details);
+header('Content-Type: application/json', true, 200);
+header('Access-Control-Allow-Origin: *',true,200);
+header('charset: utf-8',true,200);
+$rst = array();
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $myObj = new stdClass();
+        $myObj->id =  $row["sty"];
+        $myObj->description = $row["skill"];
+
+        array_push($rst, $myObj);
+        // echo json_encode($myObj);
+    }
+    // echo  implode(" ",$rst);
+    $myJSON = json_encode($rst);
+
+    echo $myJSON;
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
